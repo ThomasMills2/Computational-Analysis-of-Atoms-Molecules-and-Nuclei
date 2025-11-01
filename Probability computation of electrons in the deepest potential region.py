6cce647ca3e7fdf5aab49a6e2db0a4764f1313a2
ThomasMills2 <thomasamills25@gmail.com>
2025-11-01 20:55:48 +0000

    Update and rename 2d.py to Probability computation of electrons in the deepest potential region.py

--- Probability computation of electrons in the deepest potential region.py
+++ Probability computation of electrons in the deepest potential region.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Nov  1 20:50:47 2025
-
-@author: tom
-"""
-
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import simps
